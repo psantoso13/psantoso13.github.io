4fee734e34f868a18d1b3c9db55d921d88a37d82
psantoso13 <prayogo.santoso@gmail.com>
2023-06-01 22:23:59 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+document.getElementById('button1');
+    function a(img)
+        {
+        img.src = "img/detailOn.png"
+        }
+    function aOut(img)
+        {
+        img.src = "img/detailOff.png"
+        }
+
+document.getElementById('button2');
+    function b(img)
+        {
+        img.src = "img/certOn.png"
+        }
+    function bOut(img)
+        {
+        img.src = "img/certOff.png"
+        }
+
+document.getElementById('button3');
+    function c(img)
+        {
+        img.src = "img/contactOn.png"
+        }
+    function cOut(img)
+        {
+        img.src = "img/contactOff.png"
+        }
+
+document.getElementById('tombol4');
+    function d(img)
+        {
+        img.src = "img/emailOn.png"
+        }
+    function dOut(img)
+        {
+        img.src = "img/emailOff.png"
+        }
