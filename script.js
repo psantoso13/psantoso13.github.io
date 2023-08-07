b5b07021a69d53ce3b16536f790f7d8e223e655b
psantoso13 <prayogo.santoso@gmail.com>
2023-08-08 06:53:41 +0700

    update html - split style and script

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
 let typed = new Typed(".runningtext", {
     strings: ["- a Son a Father and a Husband", "- full time Student in the University of life", "- your partner for tougher team in your businesses"],
-    typeSpeed: 80,
+    typeSpeed: 90,
     backSpeed: 1,
     loop: true,
   });
+
+  let slideIndex = 0;
+  let timeoutId = null;
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+  
+  showSlides();
+  function currentSlide(index) {
+       slideIndex = index;
+       showSlides();
+  }
+ function plusSlides(step) {
+    
+    if(step < 0) {
+        slideIndex -= 2;
+        
+        if(slideIndex < 0) {
+          slideIndex = slides.length - 1;
+        }
+    }
+    
+    showSlides();
+ }
+  function showSlides() {
+    for(let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+      dots[i].classList.remove('active');
+    }
+    slideIndex++;
+    if(slideIndex > slides.length) {
+      slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add('active');
+     if(timeoutId) {
+        clearTimeout(timeoutId);
+     }
+    timeoutId = setTimeout(showSlides, 3000); // Change image every 3 seconds
+  }
 
 document.getElementById('button1');
     function a(img)
